2a126e314e4a38af29df57547c7a5d09ff107751
Hoots <ajvanhout@gmail.com>
2019-01-11 22:02:34 -0600

    installed discord.js

--- bot.js
+++ bot.js
@@ -2,3 +2,23 @@
 //
 //write something to make the bot log in
 //also write more to-dos
+
+//client ID = 327308932878041089
+//secret = 9OEaZRfeRzK7S7vjI7Z_0IsYZHIjJko6
+
+
+const Discord = require('discord.js');
+const client = new Discord.Client();
+const token = 'MzI3MzA4OTMyODc4MDQxMDg5.Dxr0cA.gy8e5FjLomC9FxyE_kS9jRVEiBE';
+
+client.on('ready', () => {
+  console.log(`Logged in as ${client.user.tag}!`);
+});
+
+client.on('message', msg => {
+  if (msg.content === 'ping') {
+    msg.reply('pong');
+  }
+});
+
+client.login(token);

5b05e9c843816beef586c0a3b909c988b29bb239
andyv <andyv@Hoots-Debian.home.hoots>
2019-02-03 08:54:05 -0600

    added mealidea, filled out evepricecheck stub

--- commands/help.js
+++ commands/help.js
@@ -2,6 +2,7 @@ exports.run = (bot, message, args) => {
     message.channel.send(
         "\`\`\`Available Commands:" +
         "\n!help -- Details available commands. You presumably figured this out already." +
-        "\n!evetime -- List current EVE Time \`\`\`")
+        "\n!evetime -- List current EVE Time" +
+        "\n!evepricecheck -- Currently suggests you google search the price for the thing you're wanting to price check. \`\`\`")
     .catch(console.error);
 }
--- commands/wowtime.js
+++
@@ -1,3 +0,0 @@
-exports.run = (bot, message, args) => {
-    message.reply("It doesn't matter unless you raid or are doing the fishing thing.").catch(console.error);
-}

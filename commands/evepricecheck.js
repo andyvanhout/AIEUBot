0b1e363a0246b6400960176259c1e44ffed59ee9
Andrew Van Hout <ajvanhout@gmail.com>
2019-02-08 20:06:53 -0600

    first go at evepricecheck

--- commands/evepricecheck.js
+++ commands/evepricecheck.js
@@ -1,9 +1,28 @@
-const fetch = require('node-fetch');
+const request = require("request");
 
 exports.run = (bot, message, args) => {
-    fetch("https://evepraisal.com/appraisal.json?market=jita&raw_textarea=avatar&persist=no")
-    .then(res => res.json())
-    .then(json => console.log(json));
-    message.channel.send().catch(console.error);
+    const options = { method: 'POST',
+    url: 'https://evepraisal.com/appraisal.json',
+    qs: { market: 'jita', raw_textarea: args[0], persist: 'no' },
+    headers: 
+        { 'User-Agent': 'AIEU_Discord_Bot',
+        'cache-control': 'no-cache' }
+    };
 
-}
+    request(options, function (error, response, body) {
+        if (error) throw new Error(error);
+      
+        let results = JSON.parse(body);
+        let item = results.appraisal.items[0].typeName
+        let sellPrice = results.appraisal.items[0].prices.sell.median
+        let buyPrice = results.appraisal.items[0].prices.buy.median
+
+        message.channel.reply(
+            `Item: ${item}` +
+            `\nJita Sell: ${sellPrice}` +
+            `\nJita Buy: ${buyPrice}`
+        )
+        .catch(message.channel.reply("This is not a valid item."));
+
+});
+};
